cf140d606d21fc3d44d179c9cfbb6f83a8759f2f
Arshpreet Kaur <arshpreetb417@gmail.com>
2026-07-30 21:41:53 -0500

    Configure clerk authentication.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,28 +1,8 @@
 import "dotenv/config";
-import express from "express";
-import cors from "cors";
-import resourceRoutes from "./routes/resourceRoutes";
-import assignmentRoutes from "./routes/assignmentRoutes";
+import { app } from "./app"
 
-const app = express();
-
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
-
-app.use(express.json());
-
-app.get("/", (_req, res) => {
-  res.send("StudyTrack Backend Running");
-});
-
-app.use("/api/resources", resourceRoutes);
-app.use("/api/assignments", assignmentRoutes);
-
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log('Server running on port ${PORT}');
 });
